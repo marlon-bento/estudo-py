bd608e1922ce9677840f5ab5d5320cc45757fb21
Marlon Bento <marlon_lion@hotmail.com>
2024-08-22 12:05:06 -0300

    comecando class

---
+++ Classes/classe.py
@@ -0,0 +1,7 @@
+class Myclass:
+    i = 12345
+    def f(self):
+        return 'hello world'
+x = Myclass()
+
+print(x.f())
---
+++ excecoes/try_cath.py
@@ -0,0 +1,35 @@
+import sys
+while True:
+    try:
+        x = int(input("Pelase enter a number: "))
+        break
+    except ValueError:
+        print("Opps! Favor informar somente numeros")
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
