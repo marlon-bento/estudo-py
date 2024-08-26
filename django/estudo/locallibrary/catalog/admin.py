188d2838e179f42211d54fea6171fc3a3ddf6319
Marlon Bento <marlon_lion@hotmail.com>
2024-08-26 12:07:32 -0300

    aprendendo pagina admin do django

---
+++ django/estudo/locallibrary/catalog/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+# Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ( 'first_name', 'last_name','date_of_birth', 'date_of_death')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+    
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
